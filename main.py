66737d9f978884816217fcca5b8882964157fda2
Jorge Romero C <jromerocontreras@icloud.com>
2023-06-27 00:36:10 -0400

    Validacion de datos ingresados por el usuario

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import funciones
 
 while True:
-    menu = int(input("""
+
+    try:
+        menu = int(input("""
 [1]Para agregar un libro
 [2]Para listar los libros
 [0]Para salir del programa
 >>> """))
-    if menu == 1:
-        funciones.agregar_libro()
-    elif menu == 2:
-        funciones.listar_libros()
-    elif menu == 0:
-        print("Saliendo del programa...")
-        exit()
-        
+        if menu == 1:
+            funciones.agregar_libro()
+        elif menu == 2:
+            funciones.listar_libros()
+        elif menu == 0:
+            print("Saliendo del programa...")
+            exit()
+        else:
+            print("Solo las opciones señaladas 1,2,0")
+    except ValueError as error:
+        print(f"Ingrese una opcion valida, error {error}")
